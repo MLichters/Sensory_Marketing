119de1c6785ccb60b5592c86ea060ce546a950a3
MLichters <marcel.lichters@ovgu.de>
2022-01-12 21:17:11 +0100

    Marcel

--- hearing_test_script.R
+++ hearing_test_script.R
@@ -4,15 +4,30 @@ if(!"pacman" %in% rownames(installed.packages())) install.packages("pacman")
 
 library(pacman)
 
-pacman::p_load(tidyverse, dplyr, tidyselect, osfr, rstatix, hrbrthemes)
+pacman::p_load(tidyverse, dplyr, tidyselect, osfr, rstatix, hrbrthemes, rstatix, broom)
 
 
 d <- readxl::read_xlsx("hearing test.xlsx")
 
 
+model <- lm(d$frequency ~ d$Age)
+
 ggplot(d, aes(x=Age, y=frequency, color=Gender)) + 
-  geom_point(size=6) +
   geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) +
+  annotate(geom = "text", x = 27, y = 17000, label = paste("Model : ", 
+                                                           round(model$coefficients[1], digits=2), 
+                                                           " + " , 
+                                                           round(model$coefficients[2], digits=2), 
+                                                           "*years"  , "\n\n" , "R²(adjusted) = ", 
+                                                           round(summary(model)$adj.r.squared, digits=2) 
+                                                           ) , hjust = "left") +
+  geom_point(size=6) +
   theme_ipsum()
 
 
+Frequency_ANOVA <- d %>% anova_test(frequency~ Age*Gender)
+
+print(Frequency_ANOVA, 
+      digits = 3)
+
+tibble(Frequency_ANOVA)
